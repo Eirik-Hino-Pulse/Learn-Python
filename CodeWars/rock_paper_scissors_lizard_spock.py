0257fe9ac747507c8c41d73ccc3358d9ddb5ace8
Eirik-Hino-Pulse <the.cryptopulse.global@gmail.com>
2025-11-25 13:11:04 +0000

    CodeWars 7 kyu: Easy Solution

--- CodeWars/rock_paper_scissors_lizard_spock.py
+++ CodeWars/rock_paper_scissors_lizard_spock.py
@@ -17,4 +17,9 @@ Inputs are one of 'rock', 'paper', 'scissors', 'lizard', 'spock'.
 """
 
 def rpsls(pl1, pl2):
-    pass
+    if (pl1 == "scissors" and pl2 == "paper") or (pl1 == "paper" and pl2 == "rock") or (pl1 == "rock" and pl2 == "lizard") or (pl1 == "lizard" and pl2 == "spock") or (pl1 == "spock" and pl2 == "scissors") or (pl1 == "scissors" and pl2 == "lizard") or (pl1 == "lizard" and pl2 == "paper") or (pl1 == "paper" and pl2 == "spock") or (pl1 == "spock" and pl2 == "rock") or (pl1 == "rock" and pl2 == "scissors"):
+        return "Player 1 Won!"
+    elif pl1 is pl2:
+        return "Draw!"
+    else:
+        return "Player 2 Won!"
